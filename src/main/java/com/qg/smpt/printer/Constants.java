e91bdc3f3bea4773fcd02a92e44bbbb8f14066e2
tisong <tianxin675470193@outlook.com>
2016-07-29 15:33:25 +0800

    关于大小段问题修改

--- src/main/java/com/qg/smpt/printer/Constants.java
+++ src/main/java/com/qg/smpt/printer/Constants.java
@@ -4,7 +4,7 @@ package com.qg.smpt.printer;
  * Created by tisong on 7/21/16.
  */
 public class Constants {
-    public static final int MAX_TRANSFER_SIZE = 7 * 1024;   //批次最大容量为7k
+    public static final int MAX_TRANSFER_SIZE = 1024;   //批次最大容量为7k
 
     public static final short SEND_INTERVAL = 5000;         //两次发送间隔
 }
