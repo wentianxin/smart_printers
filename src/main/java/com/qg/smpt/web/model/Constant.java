d2569f9303b0c920cfb62e35591b7fab3359ba1a
LitterWorn <503871010@qq.com>
2016-08-01 09:43:35 +0800

    增加了两个用于标识controller是否处理成功的标志位

--- src/main/java/com/qg/smpt/web/model/Constant.java
+++ src/main/java/com/qg/smpt/web/model/Constant.java
@@ -8,4 +8,7 @@ public class Constant {
 	// 操作结果
 	public static final String SUCCESS = "SUCCESS";
 	public static final String ERROR = "ERROR";
+	
+	public static final int TRUE = 1;
+	public static final int FALSE = 1;
 }
