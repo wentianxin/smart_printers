ebc4ef434c0d3fef36d9a934e550e75c932e7384
ArtemisZ <1730750312@qq.com>
2016-08-03 20:08:11 +0800

    update logo uplaod file

--- src/main/webapp/js/logo.js
+++ src/main/webapp/js/logo.js
@@ -1,103 +1,159 @@
+const WIDTH = 128;
+const HEIGHT = 128;
+var logo = {
+    oFReader: null, // 文件流
+    canvas: null, // canvas对象
+    form: null, // 获取图片表单的对象
+    left: 0,
+    top: 0,
+    strDataURI: 0,
+    getFileFullPath: function() {
+        $('#logo_upload_button').click(function() {
+            var files = document.getElementById('logo_file').files;
+            // 获取图片
+            if (files.length === 0) {
+                alert("you don't upload files!");
+                return;
+            } else {
+                logo.oFReader.readAsDataURL(files[0]);
+            }
+        });
+    },
+    _getEleLeft: function(element) {　　　　
+        var actualLeft = element.offsetLeft;　　　　
+        var current = element.offsetParent;　　　　
+        while (current !== null) {　　　　　　 actualLeft += current.offsetLeft;　　　　　　
+            current = current.offsetParent;　　　　 }　　　　
+        return actualLeft;　　 },
+    _getEleTop: function(element) {　　　　
+        var actualTop = element.offsetTop;　　　　
+        var current = element.offsetParent;　　　　
+        while (current !== null) {　　　　　　 actualTop += current.offsetTop;　　　　　　
+            current = current.offsetParent;　　　　 }　　　　
+        return actualTop;　　 },
+    // 第2步：前端获取图片文件放在canvas里面,和创建遮罩去遮盖元素
+    createCanvas: function(_this) {
+        var imgURL,
+            img,
+            canvas = _this.canvas,
+            context,
+            drag,
+            left,
+            top,
+            canvas = document.createElement('canvas');
+        canvas.id = "clip_area";
+        var c = document.createElement('canvas');
+        var area = document.getElementById('logo_clip_area');
+        if (canvas.getContext) {
+            imgURL = canvas.toDataURL("image/png");
+            img = new Image();
+            // 获取图片来源
+            img.src = _this.oFReader.result;
+            canvas.width = img.width;
+            canvas.height = img.height;
+            context = canvas.getContext('2d');
+            // 将图片插入到canvas里面
+            context.drawImage(img, 0, 0);
 
+            area.addEventListener('dragover', function(event) {
+                // 允许元素放置
+                event.preventDefault();
+            });
+            area.addEventListener('drop', function(event) {
+                var target = event.target;
+                event.preventDefault();
+                event.stopPropagation(); // 兼容ff
+                // debugger;
+                _this.left = drag.style.left = (event.pageX - left - document.getElementById('logo_clip_area').offsetLeft) + 'px';
+                _this.top = drag.style.top = (event.pageY - top - document.getElementById('logo_clip_area').offsetTop) + 'px';
+                ctx.drawImage(img, parseInt(logo.left), parseInt(logo.top), WIDTH, HEIGHT, 0, 0, WIDTH, HEIGHT); //重绘
+            });
 
-$('#logo_upload').click(function(){
-	var files = document.getElementById('logo_file').files;
-	if(files.length === 0){
-		alert("you don't upload files!");
-		return ;
-	}else{
-         	// 上传表单
-     		$.ajax({
-			url: 'user/image/1',
-			type: 'post',
-			cache: false,
-			data: new FormData($('#upload_form')[0]),
-			contentType: false,
-			processData: false,
-			dataType: 'json',
-			success: function(date){
-				// clip(data);
-				alert('success');
-			},
-			error: function(date){
-				alert('error');
-			}
-		});
-	}
-});
+            drag = document.createElement('div');
+            drag.id = "canvas_clip_area";
+            drag.draggable = "true";
+            // div拖动事件,拖动什么
+            drag.addEventListener('drag', function(event) {
+                var target = event.target;
 
-// function clip(data){
-// 	// 第一步：读取到图片的资源，然后在去加载他
-// 	var src="file:///C:/Users/st/Pictures/QQ%E5%9B%BE%E7%89%8720160703193602.jpg";
-// 	var ele = new Image();
-//     var oEndBtn = document.getElementById("clip_sure");
-    
-//     oEndBtn.addEventListener('click',function(){
-//         var x = document.getElementById("cropPosX").value, y = document.getElementById("cropPosY").value, w = document.getElementById("cropImageWidth").value, h = document.getElementById("cropImageHeight").value, angle = document.getElementById("zxxRotAngle").value;
-//         if(angle === ""){
-//             angle = 0;    
-//         }
-//         document.getElementById('test').style.backgroundPosition = x + "px  "+ y + "px";
-//         alert("角度："+angle+"\n剪裁横坐标："+x+"\n剪裁纵坐标："+y+"\n剪裁宽度："+w+"\n剪裁高度："+h);
-//     });
+                var dt = event.dataTransfer;
+                dt.dropAllowed = 'move';
+                dt.dropEffect = 'move';
+                ctx.drawImage(img, parseInt(logo.left), parseInt(logo.top), WIDTH, HEIGHT, 0, 0, WIDTH, HEIGHT); //重绘
+            });
+            drag.addEventListener('dragstart', function(event) {
+                // 允许元素放置
+                // event.preventDefault();
+                left = event.pageX - _this._getEleLeft(event.target);
+                top = event.pageY - _this._getEleTop(event.target);
+            });
+            // div 放到何处
+            drag.addEventListener('dragover', function(event) {
+                // 允许元素放置
+                event.preventDefault();
+            });
+            drag.addEventListener('dragenter', function(event) {
+                // 允许元素放置
+                event.preventDefault();
+            });
+            // 用户鼠标移除之前调用了dragenter的元素时，浏览器会触发dragleave事件
+            drag.addEventListener('dragleave', function(event) {
 
-// 	ele.src = src;
-// 	ele.id="clip_area";
-// 	document.getElementById('clip_picture').insertBefore(ele, document.getElementById('clip_picture').firstChild);
-// 	// 判定时间
-// 	window.onload = function(){
-// 	    document.getElementById("cropBeginBtn").onclick = function(){
-// 	        fnImageCropRot(ele);
-// 	        oEndBtn.style.display = "inline-block";
-// 	    };    
-// 	}
-// }
-  // jQuery(function($){
+            });
+            c.width = 300;
+            c.height = 300;
+            c.id = "show_area";
+            ctx = c.getContext('2d');
+            ctx.drawImage(img, parseInt(logo.left), parseInt(logo.top), WIDTH, HEIGHT, 0, 0, WIDTH, HEIGHT); //重绘
 
-  //   // Create variables (in this scope) to hold the API and image size
-  //   var jcrop_api,
-  //       boundx,
-  //       boundy,
+            document.getElementById('logo_clip_area').appendChild(canvas);
+            document.getElementById('logo_clip_area').appendChild(drag);
+            document.getElementById('logo_clip_area').appendChild(c);
+        }
+    },
+    // 第3步： 导出元素
+    createClip: function() {
+        var canvas = document.getElementById('show_area');
+        logo.strDataURI = canvas.toDataURL('image/png');
+        var data = logo.strDataURI.split(',')[1];
+        data = window.atob(data);
+        var ia = new Uint8Array(data.length);
+        for (var i = 0; i < data.length; i++) {
+            ia[i] = data.charCodeAt(i);
+        };
+        var blob = new Blob([ia], { type: "image/png" });
+        var form_obj = new FormData();
+        form_obj.append('file',blob);
 
-  //       // Grab some information about the preview pane
-  //       $preview = $('#preview-pane'),
-  //       $pcnt = $('#preview-pane .preview-container'),
-  //       $pimg = $('#preview-pane .preview-container img'),
+        $.ajax({
+            data: form_obj,
+            url: 'user/image/1',
+            dataType: 'json',
+            processData: false,
+            contentType: 'application/octet-stream',
+            type: 'post',
+            success: function(data) {
+                alert('success');
+            },
+            error: function(data) {
+                alert('error');
+            }
 
-  //       xsize = $pcnt.width(),
-  //       ysize = $pcnt.height();
-    
-  //   console.log('init',[xsize,ysize]);
-  //   $('#target').Jcrop({
-  //     onChange: updatePreview,
-  //     onSelect: updatePreview,
-  //     aspectRatio: xsize / ysize
-  //   },function(){
-  //     // Use the API to get the real image size
-  //     var bounds = this.getBounds();
-  //     boundx = bounds[0];
-  //     boundy = bounds[1];
-  //     // Store the API in the jcrop_api variable
-  //     jcrop_api = this;
+        });
+    },
+    init: function() {
+        var _this = this;
+        this.oFReader = new FileReader();
+        // 加载了图片之后执行这个函数
+        this.oFReader.onload = function(event) {
+            _this.createCanvas(_this);
 
-  //     // Move the preview into the jcrop container for css positioning
-  //     $preview.appendTo(jcrop_api.ui.holder);
-  //   });
+        }
+        this.getFileFullPath();
+        document.getElementById('save_file').addEventListener('click', function(event) {
+            logo.createClip();
+        });
 
-  //   function updatePreview(c)
-  //   {
-  //     if (parseInt(c.w) > 0)
-  //     {
-  //       var rx = xsize / c.w;
-  //       var ry = ysize / c.h;
-
-  //       $pimg.css({
-  //         width: Math.round(rx * boundx) + 'px',
-  //         height: Math.round(ry * boundy) + 'px',
-  //         marginLeft: '-' + Math.round(rx * c.x) + 'px',
-  //         marginTop: '-' + Math.round(ry * c.y) + 'px'
-  //       });
-  //     }
-  //   };
-
-  // });
+    }
+}
+logo.init();
